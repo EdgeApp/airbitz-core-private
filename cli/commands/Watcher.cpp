518414f3704686fc77fd3c5dc72c605894ff333c
Lucas Betschart <lucasbetschart@gmail.com>
2016-01-03 15:56:12 +0100

    Add watcher-dump, watcher-dump-file, set-stratum-server and reset-stratum-server CLI commands

--- cli/commands/Watcher.cpp
+++ cli/commands/Watcher.cpp
@@ -7,6 +7,9 @@
 
 #include "../Command.hpp"
 #include "../Util.hpp"
+#include "../../abcd/bitcoin/TxUpdater.hpp"
+#include "../../abcd/wallet/Wallet.hpp"
+#include <fstream>
 #include <unistd.h>
 #include <signal.h>
 #include <iostream>
@@ -50,5 +53,54 @@ COMMAND(InitLevel::wallet, Watcher, "watcher",
                                      session.uuid.c_str(),
                                      syncCallback, nullptr, &error));
 
+    return Status();
+}
+
+
+COMMAND(InitLevel::wallet, WatcherDump, "watcher-dump",
+        " <filename>")
+{
+    if(argc > 1)
+        return ABC_ERROR(ABC_CC_Error, helpString(*this));
+
+    if(argc == 1)
+    {
+        const std::string filename = argv[0];
+        std::ofstream file(filename);
+        if (!file.is_open())
+        {
+            return ABC_ERROR(ABC_CC_Error, "Cannot open file");
+        }
+        session.wallet->txdb.dump(file);
+    }
+    else
+        session.wallet->txdb.dump(std::cout);
+
+    return Status();
+}
+
+COMMAND(InitLevel::wallet, WatcherDumpFile, "watcher-dump-file",
+        " <filename>")
+{
+    if(argc != 1)
+        return ABC_ERROR(ABC_CC_Error, helpString(*this));
+
+    const std::string filename = argv[0];
+    std::ifstream file(filename, std::ios::in | std::ios::binary | std::ios::ate);
+    if (!file.is_open())
+    {
+        return ABC_ERROR(ABC_CC_Error, "Cannot open file");
+    }
+
+    std::streampos size = file.tellg();
+    uint8_t *data = new uint8_t[size];
+    file.seekg(0, std::ios::beg);
+    file.read(reinterpret_cast<char *>(data), size);
+    file.close();
+
+    if (!session.wallet->txdb.load(bc::data_chunk(data, data + size)))
+        return ABC_ERROR(ABC_CC_Error, "Error while loading data.");
+
+    session.wallet->txdb.dump(std::cout);
     return Status();
 }
